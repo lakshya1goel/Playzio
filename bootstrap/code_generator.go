f7870b5512c8f020b8cccdf931ada4d0a4fb3231
lakshya1goel <lakshya1234goel@gmail.com>
2025-06-19 14:17:25 +0530

    add domain for responses

---
+++ bootstrap/code_generator.go
@@ -0,0 +1,20 @@
+package bootstrap
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+func GenerateRandomCode(length int) (string, error) {
+	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
+
+	code := make([]byte, length)
+	for i := range code {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		code[i] = charset[num.Int64()]
+	}
+	return string(code), nil
+}
