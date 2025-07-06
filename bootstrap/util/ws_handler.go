346b0ce6b36ec9e2dadc64294417991b3fdbccb3
lakshya1goel <lakshya1234goel@gmail.com>
2025-07-06 19:29:27 +0530

    handle guest users for ws

--- bootstrap/util/ws_handler.go
+++ bootstrap/util/ws_handler.go
@@ -15,15 +15,9 @@ var Upgrader = websocket.Upgrader{
 }
 
 func UpgradeWithUserID(c *gin.Context) (uint, string, *websocket.Conn, bool) {
-	userIdRaw, exists := c.Get("user_id")
+	userType, exists := c.Get("user_type")
 	if !exists {
 		c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
-		return 0, "", nil, false
-	}
-
-	userId, ok := userIdRaw.(uint)
-	if !ok {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Invalid user ID in context"})
 		return 0, "", nil, false
 	}
 
@@ -33,6 +27,41 @@ func UpgradeWithUserID(c *gin.Context) (uint, string, *websocket.Conn, bool) {
 		if name, ok := userNameRaw.(string); ok {
 			userName = name
 		}
+	}
+
+	var userId uint
+
+	switch userType.(string) {
+	case "google":
+		userIdRaw, exists := c.Get("user_id")
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Missing user ID"})
+			return 0, "", nil, false
+		}
+		if id, ok := userIdRaw.(uint); ok {
+			userId = id
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Invalid user ID"})
+			return 0, "", nil, false
+		}
+
+	case "guest":
+		guestIDRaw, exists := c.Get("guest_id")
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Missing guest ID"})
+			return 0, "", nil, false
+		}
+		if guestID, ok := guestIDRaw.(string); ok {
+			userId = 0
+			userName = userName + "_" + guestID
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Invalid guest ID"})
+			return 0, "", nil, false
+		}
+
+	default:
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid user type"})
+		return 0, "", nil, false
 	}
 
 	conn, err := Upgrader.Upgrade(c.Writer, c.Request, nil)
